c30c44b93f604d326629a16fdbe8ee5f012264a9
Barack <summonbarack@gmail.com>
2025-03-04 23:57:21 +0100

    Started the design

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+// 
+
 const Navbar = () => {
   return (
-    <div>
-      <Link to="/">
-        <h1>Stock</h1>
+    <div className='absolute top-0 left-[50%] transform -translate-x-1/2 max-w-[1440px] w-full h-[120px] flex justify-between items-center text-black bg-transparent lg:px-[80px]'>
+      <Link to="/" className='text-4xl lg:text-5xl font-extrabold'> 
+        <span className='text-[#0D9A05]'>3<span className='text-[#D3A304]'>rees</span></span> 
       </Link>
+      <ul className='w-[25%] flex justify-between items-center mx-3 text-lg'>
+        <li>
+          <Link to="/">
+            <span>Link</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <span>Link</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <span>Link</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <div className='border px-2 border-black cursor-pointer rounded-md'>Explore</div>
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import React from "react"
 import { useEffect, useState } from 'react'
 
+// Components
+import Navbar from "../components/Navbar"
+
+// Images
+import HeroImg from '../assets/img1.png'
+
+
 const Home = () => {
 
   const [stocks, setStocks] = useState(null)
@@ -19,13 +26,15 @@ const Home = () => {
   }, [])
 
   return (
-    <div className='home'>
-      <div className="stock p-20">
-        {
-          stocks && stocks.map((stock) => (
-            <p key={stock._id}> {stock.name} </p>
-          ))
-        }
+    <div className='w-full h-screen relative mx-auto'>
+      <Navbar />
+      <img className="w-full h-full object-cover" src={HeroImg} />
+      <div className='w-[600px] h-auto absolute place-items-center top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 z-10'>
+        <h1 className="text-6xl font-extrabold">Interior Elegance</h1>
+        <p className="mt-4 mb-8">
+          lorem dniiind jnsvsj jsjuns bkis jdbnn skaiiidbms jdj
+        </p>
+        <button className='w-[auto] py-2 px-4 text-white px-auto bg-black cursor-pointer rounded-lg'>Explore</button>
       </div>
     </div>
   )
